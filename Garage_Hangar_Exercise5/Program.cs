f1f27505445196029e8bdab8d9bb9531c43d6d24
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-10-03 09:47:17 +0200

    Add project files.

---
+++ Garage_Hangar_Exercise5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage_Hangar_Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! We're going to create a hangar application.");
+        }
+    }
+}
