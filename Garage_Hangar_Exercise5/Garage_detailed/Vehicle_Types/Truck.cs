1f8abf69e8e53e5bc3915225d97d49ed93c3d09e
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-10-06 15:22:54 +0200

    Increased robustness of the application with try catch in case of corrupted or missing .json config

--- Garage_Hangar_Exercise5/Garage_detailed/Vehicle_Types/Truck.cs
+++ Garage_Hangar_Exercise5/Garage_detailed/Vehicle_Types/Truck.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Garage_Hangar_Exercise5.Garage_detailed.Vehicle_Types
 {
-    internal class Truck
+public class Truck : Vehicle
     {
+        public double Length { get; set; }
+
+           public Truck(string licensePlate,
+                     DateTime entryTime,
+                     DateTime? exitTime,
+                     int numberOfEngines,
+                     double engineVolume,
+                     string fuelType,
+                     string brand,
+                     double length)
+
+         : base(licensePlate,
+                entryTime,
+                exitTime,
+                numberOfEngines,
+                engineVolume,
+                fuelType,
+                brand)
+
+        {
+            Length = length;
+        }
+
+
     }
 }
--- Garage_Hangar_Exercise5/Program.cs
+++ Garage_Hangar_Exercise5/Program.cs
@@ -11,7 +11,6 @@ using System.IO;
 using Garage_Hangar_Exercise5.Garage_detailed.Vehicle_Types;
 using Garage_Hangar_Exercise5.Engine;
 
-// ...
 
 namespace Garage_Hangar_Exercise5
 {
@@ -19,35 +18,73 @@ namespace Garage_Hangar_Exercise5
     {
         static void Main(string[] args)
         {
-            // Create a configuration builder
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-            IConfigurationRoot configuration = builder.Build();
-
-            // Load billing rates into a dictionary
-            var billingRates = configuration
-                .GetSection("GarageSettings:BillingRates")
-                .Get<Dictionary<string, double>>();
-
-            // Set the default billing rate for the Vehicle class
-            if (billingRates.ContainsKey("Default"))
+            try
             {
-                Vehicle.BillingRate = billingRates["Default"];
+                // Create a configuration builder
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                IConfigurationRoot configuration = builder.Build();
+
+                // Load billing rates into the Vehicle's BillingRates dictionary
+                Vehicle.BillingRates = configuration
+                    .GetSection("GarageSettings:BillingRates")
+                    .Get<Dictionary<string, double>>() ?? new Dictionary<string, double>();
+
+                // Ensure the "Default" rate exists before trying to print it
+                if (Vehicle.BillingRates.ContainsKey("Default"))
+                {
+                    Console.WriteLine($"Default billing rate is: {Vehicle.BillingRates["Default"]}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Default billing rate is not specified in the configuration.");
+                }
+
+                // Read the garage capacity from the appsettings.json file
+                int garageCapacity = configuration.GetValue<int>("GarageSettings:Capacity");
+
+                // Create an instance of the garage with the capacity
+                Garage<Vehicle> garage = new Garage<Vehicle>(garageCapacity);
+
+                // Print the garage capacity
+                Console.WriteLine($"Capacity is: {garageCapacity}");
             }
 
-            // Print the default billing rate
-            Console.WriteLine($"Default billing rate is: {Vehicle.BillingRate}");
+            // If the configuration file is missing or corrupted, the application will continue with default values for all vehicles if users so wishes
+            catch
+            {
+                Console.WriteLine("The configuration file is missing or corrupted. What would you like to do?");
+                Console.WriteLine("1. Close application");
+                Console.WriteLine("2. Use the default values for all vehicles");
+                var choice = Console.ReadLine();
 
-            // Read the garage capacity from the appsettings.json file
-            int garageCapacity = configuration.GetValue<int>("GarageSettings:Capacity");
+                if (choice == "1")
+                {
+                Console.WriteLine("Contact the IT-support immediately to resolve the issue.");
+                    return; // Closes the application
+                }
+                if (choice == "2") 
+                Console.WriteLine("Using default values for all vehicles, contact the IT-support immediately to secure the application. ");
 
-            // Create an instance of the garage with the capacity
-            Garage<Vehicle> garage = new Garage<Vehicle>(garageCapacity);
+                // Initialize BillingRates dictionary with default values
+                Vehicle.BillingRates = new Dictionary<string, double>
+                    {
+                    {"Default", 69},
+                    {"ElectricChargeRate", 5 },
+                    {"Car", 50},
+                    {"Truck", 200},
+                    {"Airplane", 2000 },
+                    {"Bicycle", 3 },
+                    {"Boat", 350 },
+                    {"Bus", 120 },
+                    {"Motorcycle", 30 },
 
-            // Print the garage capacity
-            Console.WriteLine($"Capacity is: {garageCapacity}");
+                    // Note: Keep this updated during revisions, and add more default values for other vehicle types here if required
+                    };
+            }
+
         }
     }
 }
