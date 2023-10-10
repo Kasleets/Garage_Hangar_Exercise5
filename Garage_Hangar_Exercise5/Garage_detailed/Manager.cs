f79abc046b0b9ae6cc2e0f901fe2aaaffeca31c3
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-10-10 10:35:53 +0200

    Menu up

--- Garage_Hangar_Exercise5/Garage_detailed/Manager.cs
+++ Garage_Hangar_Exercise5/Garage_detailed/Manager.cs
@@ -16,7 +16,7 @@ namespace Garage_Hangar_Exercise5.Garage_detailed
 
         internal void Run()
         {
-            InitGarage();
+           // InitGarage();
             ShowMainMeny();
 
             //Get input from user
@@ -48,8 +48,14 @@ namespace Garage_Hangar_Exercise5.Garage_detailed
 
         private void ShowMainMeny()
         {
+            Console.WriteLine("Welcome to Garage Manager, what would you like to do?");
             Console.WriteLine("1. Park Vehicle");
-            Console.WriteLine("2. Search on reg number");
+            Console.WriteLine("2. Remove Vehicle");
+            Console.WriteLine("3. Search all cars by their license plate");
+            Console.WriteLine("4. Look for a specific car by its license plate ");
+            Console.WriteLine("5. See how many vehicles are parked inside a garage");
+            Console.WriteLine("6. Look for a specific car based on its specifics");
+            Console.WriteLine("0. Close the application");
         }
 
         private void InitGarage()
