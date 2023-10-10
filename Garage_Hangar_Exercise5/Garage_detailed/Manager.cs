2e4a0448cc675e60457537e2284e3d9ed805afa5
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-10-10 10:58:43 +0200

    Developing menu furhter based on existing code.

--- Garage_Hangar_Exercise5/Garage_detailed/Manager.cs
+++ Garage_Hangar_Exercise5/Garage_detailed/Manager.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Garage_Hangar_Exercise5;
+using Garage_Hangar_Exercise5.Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Garage_Hangar_Exercise5.Garage_detailed.Vehicle_Types;
 
 namespace Garage_Hangar_Exercise5.Garage_detailed
 {
@@ -23,12 +26,93 @@ namespace Garage_Hangar_Exercise5.Garage_detailed
             string input = Console.ReadLine();
             switch (input)
             {
-                case "1":
+                    case "1":
                     ParkVehicle();
                     break;
+
+                    case "2":
+                    RemoveVehicle();
+                    break;
+
+                    case "3":
+                    SearchAllVehiclesByLicensePlate();
+                    break;
+
+                    case "4":
+                    SearchSpecificVehicleByLicensePlate();
+                    break;
+
+                    case "5":
+                    DisplayNumberOfParkedVehicles();
+                    break;
+
+                    case "6":
+                    SearchVehiclesByProperties();
+                    break;
+
+                    case "0":
+                    Environment.Exit(0);
+                    break;
+
                 default:
+                    Console.WriteLine("Invalid input, please try again");
                     break;
             }
+        }
+
+        private void SearchVehiclesByProperties()
+        {
+            var properties = UI.DisplaySearchMenu();
+            var criteria = UI.CaptureCriteria(garage, properties);
+            var vehicles = garage.FindVehicles(v =>
+            {
+                foreach (var property in criteria.Keys)
+                {
+                    var value = v.GetType().GetProperty(property)?.GetValue(v)?.ToString();
+                    if (!criteria[property].Contains(value))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            Console.WriteLine($"Found {vehicles.Count()} vehicles based on the given criteria.");
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+
+        private void DisplayNumberOfParkedVehicles()
+        {
+            var allParkedVehicles = garage.GetAllParkedVehicles();
+            Console.WriteLine($"Total number of parked vehicles: {allParkedVehicles.Count()}");
+
+        }
+
+        private void SearchSpecificVehicleByLicensePlate()
+        {
+            Console.WriteLine("Enter the license plate of the vehicle you're looking for:");
+            string licensePlate = Console.ReadLine();
+            var vehicle = garage.GetVehicle(licensePlate);
+            if (vehicle != null)
+            {
+                Console.WriteLine($"Found vehicle: {vehicle}");
+            }
+            else
+            {
+                Console.WriteLine("No vehicle found with the given license plate.");
+            }
+        }
+
+        private void SearchAllVehiclesByLicensePlate()
+        {
+            garage.ListAllParkedVehicles();
+        }
+
+        private void RemoveVehicle()
+        {
+            throw new NotImplementedException();
         }
 
         private void ParkVehicle()
