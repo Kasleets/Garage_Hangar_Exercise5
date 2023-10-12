2633572e84058943cbfd6b9a01116bb192d6c58e
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-10-12 12:46:47 +0200

    More xUnit Tests

--- xUnitGarage/UnitTest1.cs
+++ xUnitGarage/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace xUnitGarage
         {
             // Arrange
 
-            var garage = new Garage<Vehicle>(30); // Assuming a capacity of 30 cars for this test
+            var garage = new Garage<Vehicle>(10); // Assuming a capacity of 10 vehicles for this test
             var car = new Car("UTI123", DateTime.Now, null, 1, 3900, "Petrol", "Mazda", "Black");
 
 
@@ -38,7 +38,7 @@ namespace xUnitGarage
         {
             // Arrange
 
-            var garage = new Garage<Vehicle>(30); // Assuming a capacity of 30 cars for this test
+            var garage = new Garage<Vehicle>(10); // Assuming a capacity of 30 cars for this test
             var bus = new Bus("KOT123", DateTime.Now, null, 1, 3900, "Petrol", "Mazda", 24, "Black");
 
 
@@ -66,7 +66,7 @@ namespace xUnitGarage
         public void Should_ParkVehicles_Successfully(string vehicleType, string licensePlate, int? numberOfSeats, int? wingSpan)
         {
             // Arrange
-            var garage = new Garage<Vehicle>(30);
+            var garage = new Garage<Vehicle>(10);
 
             Vehicle vehicle;
 
@@ -95,6 +95,27 @@ namespace xUnitGarage
             Assert.NotNull(retrievedVehicle);
             Assert.Equal(vehicle.LicensePlate, retrievedVehicle.LicensePlate);
         }
+
+        [Fact]
+        public void Should_FailToParkVehicle_WhenGarageIsFull()
+        {
+            // Arrange
+            var garage = new Garage<Vehicle>(1); // Creating a garage with capacity of only 1 vehicle
+            var firstCar = new Car("CAR123", DateTime.Now, null, 1, 3900, "Petrol", "Brand", "Black");
+            var secondCar = new Car("CAR456", DateTime.Now, null, 1, 3900, "Petrol", "Brand", "Red");
+
+            // Act
+            bool isFirstCarParked = garage.ParkVehicle(firstCar);
+            bool isSecondCarParked = garage.ParkVehicle(secondCar); // This should fail since the garage is full
+
+            // Assert
+
+            // Flip Assert.True to Assert.False to see the test fail to confirm that the conditions are correct
+            Assert.True(isFirstCarParked);
+            Assert.False(isSecondCarParked); // Expecting this to be false since parking the second car should fail
+        }
+
+
 
     }
 }
